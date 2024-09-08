87eab15bbb66b05719cf3fdf3f9ccaa622b42da9
dwilso21 <dwilso21@uccs.edu>
2024-09-08 16:08:00 -0600

    Update eval_all.sh
    updated checkpoint path name

--- eval_all.sh
+++ eval_all.sh
@@ -3,7 +3,7 @@ for((i=0;i<=9;i++));
 do   
     # echo $i
     TXT_PATH='/content/drive/MyDrive/GMM_DW/results/test_'"$i"'.txt'
-    CKPT_PATH='/content/drive/MyDrive/GMM_DW/trained model output/'"$i"'/checkpoint_4.pth'
+    CKPT_PATH='/content/drive/MyDrive/GMM_DW/model_output/'"$i"'/checkpoint_4.pth'
     python /content/GMM_DW/batch_eval.py --cfg-path /content/GMM_DW/eval_configs/minigpt4_eval_all_tasks_imgr.yaml \
     --gpu-id 0 --task-id $i --txt-path $TXT_PATH \
     --ckpt-path $CKPT_PATH
