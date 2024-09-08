cd9c21cf9de56548b812905369e3515a5db3c7a2
dwilso21 <dwilso21@uccs.edu>
2024-09-08 15:50:46 -0600

    Update eval_all.sh
    updated batch_eval paths

--- eval_all.sh
+++ eval_all.sh
@@ -4,8 +4,8 @@ do
     # echo $i
     TXT_PATH='/content/drive/MyDrive/GMM_DW/results/test_'"$i"'.txt'
     CKPT_PATH='/content/drive/MyDrive/GMM_DW/trained model output/'"$i"'/checkpoint_4.pth'
-    python batch_eval.py --cfg-path eval_configs/minigpt4_eval_all_tasks_imgr.yaml \
+    python /content/GMM_DW/batch_eval.py --cfg-path /content/GMM_DW/eval_configs/minigpt4_eval_all_tasks_imgr.yaml \
     --gpu-id 0 --task-id $i --txt-path $TXT_PATH \
     --ckpt-path $CKPT_PATH
 done  
-python get_score_all.py
+python /content/GMM_DW/get_score_all.py
